23682e7a0b2d8a0d0ad959dc00d0db2147200647
RICARDO AZZI SILVA <ricardo.azzi-ext@viavarejo.com.br>
2020-07-12 19:02:30 -0300

    Mudando a estrutura do sistema porque o hostgator não me deixa mexer no arquivo httpd, as configurações do apache >____>

--- public_html/config.php
+++ public_html/config.php
@@ -3,14 +3,14 @@
 date_default_timezone_set("America/Sao_Paulo");
 
 define("APP_BASE", "$_SERVER[DOCUMENT_ROOT]/../app/");
-define("BOOK_PATH", "$_SERVER[DOCUMENT_ROOT]/../books/");
-define("ARCHIVE_IMAGES_PATH", "$_SERVER[DOCUMENT_ROOT]/www/image-archive/");
+define("BOOK_PATH", "$_SERVER[DOCUMENT_ROOT]/../app/books/");
+define("ARCHIVE_IMAGES_PATH", "$_SERVER[DOCUMENT_ROOT]/public_html/image-archive/");
 define("TEMPLATE_PATH", APP_BASE."templates/");
 define("TEMPLATE_CACHE_PATH", TEMPLATE_PATH."cache/");
 
 function __autoload($class) {
 	$class = strtolower($class);
-	require_once("$_SERVER[DOCUMENT_ROOT]/../app/service/$class.class.php");
+	require_once(APP_BASE."service/$class.class.php");
 }
 
 ?>
